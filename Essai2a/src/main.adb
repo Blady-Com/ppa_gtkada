f8991284cf21c4d2b4e5bb7cc75caccc7eb0fc81
Blady <blady.net@orange.fr>
2021-07-15 21:28:07 +0200

    Essai2a: Add_From_File profil has changed. Get_Widget is replaced by Get_Object with Gtk_Builder type. Gtk.Object is obsolete.

--- Essai2a/src/main.adb
+++ Essai2a/src/main.adb
@@ -7,16 +7,16 @@ with Ada.Text_IO;
 
 procedure Main is
    Builder          : Gtkada_Builder;
-   Error            : GError;
+   Error            : aliased Glib.Error.GError;
    Default_Filename : constant String := "Essai2.glade";
+   use Glib;
 begin
    --  Initialize GtkAda.
    Gtk.Main.Init;
 
    --  Create from UI description
    Gtk_New (Builder);
-   Error := Add_From_File (Builder, Default_Filename);
-   if Error /= null then
+   if Add_From_File (Builder, Default_Filename, Error'Access) = 0 then
       Ada.Text_IO.Put_Line ("Error : " & Get_Message (Error));
       Error_Free (Error);
       return;
@@ -34,7 +34,7 @@ begin
       Handler_Name => "on_button2_clicked",
       Handler      => On_Button2_Clicked'Access);
    Do_Connect (Builder);
-   Show_All (Get_Widget (Builder, "window1"));
+   Show_All (Gtk_Widget (Get_Object (Builder, "window1")));
 
    --  Start the Gtk+ main loop
    Gtk.Main.Main;
--- Essai2a/src/window1_callbacks.adb
+++ Essai2a/src/window1_callbacks.adb
@@ -1,5 +1,4 @@
 with Glib;                        use Glib;
-with Gtk.Object;                  use Gtk.Object;
 with Gtk.Enums;                   use Gtk.Enums;
 with Gtk.Main;
 with Ada.Long_Long_Float_Text_IO;
@@ -26,7 +25,7 @@ package body Window1_Callbacks is
    ------------------------
 
    procedure On_Button2_Clicked (Builder : access Gtkada_Builder_Record'Class) is
-      S : constant UTF8_String := Get_Text (Gtk_Entry (Get_Widget (Builder, "entry1")));
+      S : constant UTF8_String := Get_Text (Gtk_Entry (Get_Object (Builder, "entry1")));
       function Factorielle (N : Natural) return Long_Long_Integer is
          F : Long_Long_Integer := 1;
       begin
@@ -45,21 +44,21 @@ package body Window1_Callbacks is
          Ada.Long_Long_Float_Text_IO.Put
            (S,
             F,
-            Integer (Gtk_Hscale (Get_Widget (Builder, "hscale1")).Get_Value),
+            Integer (Gtk_Hscale (Get_Object (Builder, "hscale1")).Get_Value),
             6);
          return S;
       end Factorielle;
    begin
-      if Gtk_Check_Button (Get_Widget (Builder, "checkbutton1")).Get_Active then
+      if Gtk_Check_Button (Get_Object (Builder, "checkbutton1")).Get_Active then
          Set_Text
-           (Gtk_Label (Get_Widget (Builder, "label3")),
+           (Gtk_Label (Get_Object (Builder, "label3")),
             Long_Long_Integer'Image (Factorielle (Natural'Value (S))));
       else
-         Set_Text (Gtk_Label (Get_Widget (Builder, "label3")), Factorielle (Natural'Value (S)));
+         Set_Text (Gtk_Label (Get_Object (Builder, "label3")), Factorielle (Natural'Value (S)));
       end if;
    exception
       when others =>
-         Set_Text (Gtk_Label (Get_Widget (Builder, "label3")), "Erreur!");
+         Set_Text (Gtk_Label (Get_Object (Builder, "label3")), "Erreur!");
    end On_Button2_Clicked;
 
    -----------------------------

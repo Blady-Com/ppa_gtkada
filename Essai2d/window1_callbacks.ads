5f67e0b867eab03af7488b60be0d3313b7a1d403
Blady <blady.net@orange.fr>
2021-07-17 11:33:46 +0200

    Add Essai2d (GTKAda CE 2021).

---
+++ Essai2d/window1_callbacks.ads
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------------
+--  Legal licensing note : !!! Edit the file gate3_license.txt !!!
+--
+--  Copyright (c) F. J. FABIEN - 2013
+--  Berry
+--  FRANCE
+--  Send bug reports or feedback to : francois_fabien@hotmail.com
+--
+--  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+--  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+--  DEALINGS IN THE SOFTWARE.
+--  NB: this is the MIT License, as found 12-Sep-2007 on the site
+--  http://www.opensource.org/licenses/mit-license.php
+-----------------------------------------------------------------------------
+with Gtkada.Builder; use Gtkada.Builder;
+
+package Window1_Callbacks  is
+
+   function On_Window1_Delete_Event  (Builder : access Gtkada_Builder_Record'Class)
+      return Boolean;
+
+   procedure On_Button2_Clicked (Builder : access Gtkada_Builder_Record'Class);
+
+   procedure On_Button1_Clicked (Builder : access Gtkada_Builder_Record'Class);
+
+
+end Window1_Callbacks;
